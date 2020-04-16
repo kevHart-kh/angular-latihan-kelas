e1d1fbe3cdfd3ab527ed0e8cf56325635643605c
kevHart-kh <c14180077@john.petra.ac.id>
2020-04-16 19:40:18 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+
+  a = 0;
+  b = 0;
+  sum = 1;
+  total=[];
+  str='';
+
+  reset(){
+    this.a=0;
+    this.b=0;
+    this.total=[];
+    this.sum = 0;
+  }
+
+  
+
 }
